8e686ddd7e28e2d676a95831a79e0e0d7501674c
Sergey <gck.sergey@gmail.com>
2018-09-24 16:19:02 +0300

    kak eto rabotaet k3k

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+	let a = 0, b = 0;
+	for(let i = 0; i<9; i++){
+		for(let j = 0; j<9;j++){
+			let c = 1;
+			if(matrix[i][j]==0){
+				while(matrix[i][j]!=c){
+					if(i<3) { a = 0 }
+						else if(i<6){ a = 3 }
+							else { a = 6 }
+					if(j<3) { b = 0 }
+						else if(j<6){ b = 3 }
+							else { b = 6 }
+					if(checkMatrix(a,b,c)&&checkCol(j,c)&&checkRow(i,c)){
+						matrix[i][j] = c;
+					}
+					else{
+						c++;
+					}
+				}
+			}
+		}
+	}
+	return matrix;
+
+
+	function checkMatrix(a,b,c){
+		for(let i = a; i<a+3; i++){
+			for(let j = b; j<b+3; j++){
+				if(matrix[i][j]==c) return false;
+			}
+		}
+		return true;
+	}
+	function checkCol(j,c){
+		for(let i = 0; i<9; i++){
+			if(matrix[i][j]==c) return false;
+		}
+		return true;
+	}
+	function checkRow(i,c){
+		for(let j = 0; j<9; j++){
+			if(matrix[i][j]==c) return false;
+		}
+		return true;
+	}
 }
